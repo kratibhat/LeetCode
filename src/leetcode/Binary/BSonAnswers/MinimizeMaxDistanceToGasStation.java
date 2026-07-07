44943565eec3b3bd65c38bb5ac144f9f420efdb0
kratibhat <bhatkrati@gamil.com>
2026-07-07 22:17:56 +0530

    BASIC CODING

--- src/leetcode/Binary/BSonAnswers/MinimizeMaxDistanceToGasStation.java
+++ src/leetcode/Binary/BSonAnswers/MinimizeMaxDistanceToGasStation.java
@@ -1,5 +1,24 @@
 package leetcode.Binary.BSonAnswers;
-
+//Problem Statement: You are given a sorted array ‘arr’ of length ‘n’,
+// which contains positive integer positions of ‘n’ gas stations on the X-axis. \
+// You are also given an integer ‘k’. You have to place 'k' new gas stations on the X-axis.
+// You can place them anywhere on the non-negative side of the X-axis, even on non-integer
+// positions. Let 'dist' be the maximum value of the distance between adjacent gas stations after
+// adding k new gas stations. Find the minimum value of ‘dist’.
+//
+//Examples
+//Example 1:
+//Input Format: N = 5, arr[] = {1,2,3,4,5}, k = 4
+//Result: 0.5
+//Explanation: One of the possible ways to place 4 gas stations is {1,1.5,2,2.5,3,3.5,4,4.5,5}.
+// Thus the maximum difference between adjacent gas stations is 0.5.
+// Hence, the value of ‘dist’ is 0.5. It can be shown that there is no possible way to add
+// 4 gas stations in such a way that the value of ‘dist’ is lower than this.
+//
+//Example 2:
+//Input Format: N = 10, arr[] = {1,2,3,4,5,6,7,8,9,10}, k = 1
+//Result: 1
+//Explanation: One of the possible ways to place 1 gas station is {1,1.5,2,3,4,5,6,7,8,9,10}. Thus the maximum difference between adjacent gas stations is still 1. Hence, the value of ‘dist’ is 1. It can be shown that there is no possible way to add 1 gas station in such a way that the value of ‘dist’ is lower than this.
 // LeetCode 774 - Minimize Max Distance to Gas Station
 // Given positions of gas stations and K extra stations to add, minimize the maximum distance
 // between adjacent stations after adding K stations. Return minimized maximum distance.
